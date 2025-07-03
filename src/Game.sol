c7c4d40e52608df4436175a68006cba60b4549bd
awn <siddiqui4wn@gmail.com>
2025-07-03 22:48:01 +0530

    Change in game logic

--- src/Game.sol
+++ src/Game.sol
@@ -1,84 +1,85 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
+pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import {VRFCoordinatorV2Interface} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-contract GameRules is VRFConsumerBaseV2{
-    address public immutable croupier;
 
-    struct Participant {
-        address gambler;
-        uint guess;
-    }
-    struct GameRound {
-        uint guessingWindowTime;
-        uint range;
-        uint correct; 
-        uint fees;
-    }
+contract GameStructure is VRFConsumerBaseV2{
+    event RequestedRandomness(uint256 requestId);
+    event RequestFulfilled(uint256 round);
 
-    uint public constant FEE = 1 ether;
-
-    GameRound[] public gameRounds;
-    uint public roundNumber = 1;
-
-    constructor(
-    address _vrfCoordinator,
-    bytes32 _keyHash,
-    uint64 _subId ) VRFConsumerBaseV2(_vrfCoordinator) {
-
-        croupier = msg.sender;
-
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
-        keyHash = _keyHash;
-        subscriptionId = _subId;
-    }
-
-    //round number to secret number
-
-    VRFCoordinatorV2Interface COORDINATOR;
+    address public immutable i_croupier;
+    uint256 internal constant FEE = 1 ether;
+    uint256 internal immutable i_interval; // interval between rounds in seconds
+    uint256 public immutable i_startTime; // whenever the game starts 
 
     //below are the CHAINLINK VRF VARIABLES 
-    bytes32 private keyHash; //max gas willing to pay for chainlink oracle's job
-    uint64 private subscriptionId; // LINK-funded sub ID
+    bytes32 private immutable i_keyHash; //max gas willing to pay for chainlink oracle's job
+    uint64 private immutable i_subscriptionId; // LINK-funded sub ID
     uint16 private constant REQUEST_CONFIRMATIONS = 3; // wait time (usually 3 for testing)
     uint32 private constant CALLBACK_GAS_LIMIT = 100000; //max gas chainlink node can use while calling func
     uint32 private constant NUM_WORDS = 1; // number of random words to be generated
 
-    mapping (uint => uint) secretNumberOfRound;
-    mapping (uint => bool) secretNumberIsReady;
+    //we will be using the interface to implement the consumer base
+    VRFCoordinatorV2Interface private COORDINATOR;
+
+
+    struct GameRound {
+        uint8 roundNumber;
+        uint256 guessingWindowTime;
+        uint256 range;
+        uint256 fees; 
+    }
+
+    GameRound[6] public s_gameRounds;
+
+    constructor(address _vrfCoordinator, bytes32 keyHash, uint64 subscriptionId) VRFConsumerBaseV2(_vrfCoordinator) {
+
+        i_croupier = msg.sender;
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+
+    }
+
+    //round number to secret number
+    
+    mapping (uint256 => uint256) private secretNumberOfRound;
+    mapping (uint256 => bool) public secretNumberIsReady;
+    // mapping () private s_roundInitialised;
 
 
     //first we will send a request for generating random number;
-    function requestRandomNumber() external {
-        require(!secretNumberIsReady[roundNumber]);
-        require(msg.sender == croupier);
+    // calling this external function via the interface
+    function requestRandomWords() external {
+        // require(!secretNumberIsReady[roundNumber], "Secret number already generated for this round");
+        require(msg.sender == i_croupier, "Only croupier can request random words");
         COORDINATOR.requestRandomWords(
-            keyHash,
-            subscriptionId,
-            REQUEST_CONFIRMATIONS,
+            i_keyHash,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, /* (blockConfirmations/ timeForOracleToRespond) */
             CALLBACK_GAS_LIMIT,
             NUM_WORDS
         );
     }
 
     function fulfillRandomWords(uint256, uint256[] memory randomNumber) internal override {
-        uint difficulty = 10**roundNumber;
-        uint secret = (randomNumber[0] % difficulty);
+        
+        uint256 secretNumber;
+        for(uint8 i=1; i<=s_gameRounds.length; i++){
 
-        secretNumberOfRound[roundNumber] = secret;
-        secretNumberIsReady[roundNumber] = true;
+            uint256 difficulty = 10**i;
+            uint256 secretNumber = (randomNumber[0] % difficulty);
 
+            secretNumberOfRound[i] = secretNumber;
+            secretNumberIsReady[i] = true;
 
-        GameRound memory gameRound = GameRound({
-            guessingWindowTime: roundNumber*10,
-            range: difficulty,
-            correct: secret,
-            fees: FEE*roundNumber
-        });
-
-        gameRounds.push(gameRound);
-        roundNumber++;
+            s_gameRounds[i] = GameRound({
+                roundNumber: i+1,
+                guessingWindowTime: i*10,
+                range: difficulty,
+                fees: FEE*i
+            });
+        }
     }
 }
+
--- src/PlayGame.sol
+++ src/PlayGame.sol
@@ -1,51 +1,146 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {GameRules} from "../src/Game.sol";
+// import {GameStructure} from "../src/Game.sol";
+import {VRFCoordinatorV2Interface} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
-contract NewGame is GameRules {
 
-    struct Participant {
-        uint guess;
+interface IErrors {
+    error AlreadyInGame();
+    error FeeNotPaid();
+    error MustEnterGameFirst();
+    error AlreadyPooledMoney();
+    error PoolingWindowClosed();
+    error OnlyCroupierCanCall();
+    error GuessingWindowClosed();
+    error GuessingWindowNotClosed();
+}
+
+interface IGameEvents {
+    event PlayerEntered(address indexed player, uint256 time);
+    event NewRoundStartingIn(uint256 roundNumber, uint256 timeInterval);
+    event NewRoundStarted(uint256 roundNumber, uint256 remainingTime);
+    event PlayerProposedGuess(address indexed player, uint256 guess, uint256 roundNumber);
+}
+
+contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
+
+    address payable[] private s_players;
+    uint256 private moneyPool;
+
+    event RequestedRandomness(uint256 requestId);
+    event RequestFulfilled(uint256 round);
+
+    address public immutable i_croupier;
+    uint256 internal constant FEE = 1 ether;
+    uint256 internal immutable i_interval; // interval between rounds in seconds
+    uint256 public immutable i_startTime; // whenever the game starts 
+
+    //below are the CHAINLINK VRF VARIABLES 
+    bytes32 private immutable i_keyHash; //max gas willing to pay for chainlink oracle's job
+    uint64 private immutable i_subscriptionId; // LINK-funded sub ID
+    uint16 private constant REQUEST_CONFIRMATIONS = 3; // wait time (usually 3 for testing)
+    uint32 private constant CALLBACK_GAS_LIMIT = 100000; //max gas chainlink node can use while calling func
+    uint32 private constant NUM_WORDS = 1; // number of random words to be generated
+
+    //we will be using the interface to implement the consumer base
+    VRFCoordinatorV2Interface private COORDINATOR;
+
+    struct GameRound {
+        uint8 roundNumber;
+        uint256 guessingWindowTime;
+        uint256 range;
+        uint256 fees; 
     }
 
-    constructor(address _croupier) GameRules(_croupier){
+    GameRound[6] public s_gameRounds;
 
+    mapping (uint256 => uint256) private secretNumberOfRound;
+    mapping (uint256 => bool) public secretNumberIsReady;
+    mapping (uint8 => bool) private s_roundStarted;
+
+    constructor(address _vrfCoordinator, bytes32 keyHash, uint64 subscriptionId) VRFConsumerBaseV2(_vrfCoordinator) {
+
+        i_croupier = msg.sender;
+
+        i_keyHash = keyHash;
+        i_subscriptionId = subscriptionId;
+        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
     }
 
-    enum Gamble {Out, In}
-    //Gamble.In is stored as 1 and Gamble.Out is stored as 0 so Out is default!
+    modifier onlyCroupier() {
+        require(msg.sender == i_croupier, OnlyCroupierCanCall());
+        _;
+    }
 
-    mapping (address=> mapping(uint => bool)) public isPlayingThisRound;
-    // player to round number to playingornot
-    mapping (address => Gamble) enteredGame;
+    mapping (address => mapping(uint8 => uint256)) private s_playerGuessForRound;
+    // player to round number to guess
+    mapping (address => bool) private enteredGame;
 
     function enterGame() external payable {
-        require(enteredGame[msg.sender] == Gamble.out, "already in game");
-        require(msg.value == FEE, "must pay participating fee");
+        require(enteredGame[msg.sender] == false, AlreadyInGame());
+        require(msg.value == FEE, FeeNotPaid());
 
-        enteredGame[msg.sender] = Gamble.In;
+        enteredGame[msg.sender] = true;
+        s_players.push(payable(msg.sender));
+        moneyPool += msg.value;
+
+        emit PlayerEntered(msg.sender, block.timestamp);
     }
 
-    function startGambling(uint roundNumber, uint guess) external payable {
-        require(enteredGame[msg.sender] == Gamble.In, "must enter game first");
+    function startRound(uint8 roundNumber) external onlyCroupier {
+        require(block.timestamp == i_startTime + (roundNumber * i_interval), "Too early to start round");
+        require(!s_roundStarted[roundNumber], "Round already started");
 
-        for(uint i=0; i< gameRounds.length; i++){
-            roundNumber = i+1;
-            require(!isPlayingThisRound[msg.sender][i+1], "already pooled money");
-            require(block.timestamp<= gameRounds[i].guessingWindowTime, "Pooling windo closed!");
+        require(msg.sender == i_croupier, "Only croupier can request random words");
+        uint256 requestId = COORDINATOR.requestRandomWords(
+            i_keyHash,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS, /* (blockConfirmations/ timeForOracleToRespond) */
+            CALLBACK_GAS_LIMIT,
+            NUM_WORDS
+        );
 
-            (bool depositedToPool, ) = address(this).call{value: FEE*(i)}("");
-            require( depositedToPool);
+        s_roundStarted[roundNumber] = true;
+    }
+    
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        uint256 secretNumber;
+        for(uint8 i=1; i<=s_gameRounds.length; i++){
+
+            uint256 difficulty = 10**i;
+            uint256 secretNumber = (randomNumber[0] % difficulty);
+
+            secretNumberOfRound[i] = secretNumber;
+            secretNumberIsReady[i] = true;
+
+            s_gameRounds[i] = GameRound({
+                roundNumber: i+1,
+                guessingWindowTime: i*10,
+                range: difficulty,
+                fees: FEE*i
+            });
         }
-        isPlayingThisRound[msg.sender][i+1] = true;
-
-        address(this).balance += msg.value;
-
-
     }
 
-    function proposeGuess(uint _guess) external {
 
+    function enterRoundproposeGuesses(uint8 round, uint256 guess) external payable{ 
+        round = round - 1; // appropriate indexing for array
+        GameRound memory gameRound = s_gameRounds[round];
+        s_gameRounds[round].roundNumber = round +1;
+
+        require(enteredGame[msg.sender], MustEnterGameFirst());
+        require(s_roundStarted[0], "Round not started yet");
+
+        require(block.timestamp < i_startTime + (gameRound[round]*i_interval) 
+        && block.timestamp > i_startTime + ((gameRound[round]-1)*i_interval), GuessingWindowClosed());
+
+        require(msg.value == gameRound.fees, FeeNotPaid());
+
+        require(s_playerGuessForRound[msg.sender][round] == 0, "Already proposed a guess for this round");
+
+        s_playerGuessForRound[msg.sender][round] = guess;
+        emit PlayerProposedGuess(msg.sender, guess, round + 1);
     }
 }

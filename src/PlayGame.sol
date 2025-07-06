12c9ec92863405bf5f15bc96040165a7339d1e1c
awn <siddiqui4wn@gmail.com>
2025-07-06 23:51:33 +0530

    added automation

--- src/PlayGame.sol
+++ src/PlayGame.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.30;
 // import {GameStructure} from "../src/Game.sol";
 import {VRFCoordinatorV2Interface} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "../lib/chainlink-evm/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+// import {AutomationCompatible} from "../lib/chainlink-evm/contracts/src/v0.8/automation/AutomationCompatible.sol";
 
 
 interface IErrors {
@@ -26,9 +27,10 @@ interface IGameEvents {
     event RequestedRandomness(uint256 requestId);
     event RequestFulfilled(uint256 round);
     event WinnersListed(uint8 roundNumber, address[] winners);
+    event WinnerRewarded(uint8 roundNumber, address[] winners, uint256 reward);
 }
 
-contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
+contract NewGame is /* AutomationCompatible */ VRFConsumerBaseV2, IErrors, IGameEvents {
 
     address payable[] private s_players;
 
@@ -38,20 +40,26 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
     uint256 public immutable i_startTime; // whenever the game starts 
     uint256 public immutable i_announceTime;
 
-    //below are the CHAINLINK VRF VARIABLES 
-    bytes32 private immutable i_keyHash; //max gas willing to pay for chainlink oracle's job
-    uint64 private immutable i_subscriptionId; // LINK-funded sub ID
-    uint16 private constant REQUEST_CONFIRMATIONS = 3; // wait time (usually 3 for testing)
-    uint32 private constant CALLBACK_GAS_LIMIT = 100000; //max gas chainlink node can use while calling func
-    uint32 private constant NUM_WORDS = 1; // number of random words to be generated
+    /* below are the CHAINLINK VRF VARIABLES */
+    bytes32 private immutable i_keyHash;                    /* max gas willing to pay for chainlink oracle's job */
+    uint64 private immutable i_subscriptionId;              /* LINK-funded sub ID */ 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;      /* wait time (usually 3 for testing) */
+    uint32 private constant CALLBACK_GAS_LIMIT = 100000;    /* max gas chainlink node can use while calling func */ 
+    uint32 private constant NUM_WORDS = 1;                  /* number of random words to be generated */ 
 
     //we will be using the interface to implement the consumer base
     VRFCoordinatorV2Interface private COORDINATOR;
 
+    mapping (uint8 => address payable[]) private s_playersInRound;
     mapping (uint256 => uint256) private secretNumberOfRound;
     mapping (uint256 => bool) public secretNumberIsReady;
     mapping (uint8 => bool) private s_roundStarted;
     mapping (uint8 => uint256) public moneyPoolForRound;
+    mapping (uint8 => address[]) public winnersForRound; 
+    mapping (uint256 => uint8) public requestIdForRound; 
+    mapping (address => mapping(uint8 => uint256)) private s_playerGuessForRound;
+    mapping (address => bool) private enteredGame;
+
 
     constructor(address _vrfCoordinator, bytes32 keyHash, uint64 subscriptionId) VRFConsumerBaseV2(_vrfCoordinator) {
 
@@ -67,17 +75,25 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
         _;
     }
 
-    mapping (address => mapping(uint8 => uint256)) private s_playerGuessForRound;
-    // player to round number to guess
-    mapping (address => bool) private enteredGame;
+    function checkUpKeep(bytes calldata checkData) public view returns (bool upKeepNeeded, bytes memory returnInfo) {
 
-    function enterGame() external payable {
+    } 
+
+    function performUpKeep(bytes calldata) public view {
+
+    }
+
+    function enterGame(uint256 guessForRound1) external payable {
         require(enteredGame[msg.sender] == false, AlreadyInGame());
         require(msg.value == FEE, FeeNotPaid());
         require(block.timestamp< i_startTime, WindowClosed());
+        require(guessForRound1>0 && guessForRound1<10, InvalidProposal());
+
+        s_playerGuessForRound[msg.sender][1] = guessForRound1;
 
         enteredGame[msg.sender] = true;
         s_players.push(payable(msg.sender));
+        s_playersInRound[1].push(payable(msg.sender));
         moneyPoolForRound[1] += msg.value;
 
         emit PlayerEntered(msg.sender, block.timestamp);
@@ -95,6 +111,8 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
         s_playerGuessForRound[msg.sender][roundNumber] = guess;
         moneyPoolForRound[roundNumber] += msg.value;
 
+        s_playersInRound[roundNumber].push(payable(msg.sender));
+
         emit PlayerProposedGuess(msg.sender, guess, roundNumber);
     }
 
@@ -111,12 +129,12 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
             NUM_WORDS
         );
 
+        requestIdForRound[requestId] = roundNumber;
+
         s_roundStarted[roundNumber] = true;
     }
-
-    mapping (uint8 => address[]) public winnersForRound; // round to winners list
     
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    function fulfillRandomWords(uint256 /*requestId*/, uint256[] memory randomWords) internal override {
         uint256 secretNumber;
         for(uint8 i=1; i<=6; i++){
 
@@ -125,6 +143,8 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
 
             secretNumberOfRound[i] = secretNumber;
             secretNumberIsReady[i] = true;
+
+            s_playersInRound[i] = new address payable[](0);
         }
     }
 
@@ -168,14 +188,22 @@ contract NewGame is VRFConsumerBaseV2, IErrors, IGameEvents {
     }
 
     function rewardWinners() private {
-        // for each round r, 
-        // and for i in each winner in winnersForRound[r]
-        // winnersForRound[r][i].call{value: (moneyPoolForRound[r] - GasCostForUsingVRF)/winnersForRound[r].length}(""); 
-
         for (uint8 r=1; r<6; r++){
+            uint256 fullReward = (moneyPoolForRound[r])/winnersForRound[r].length;
+            uint256 halfReward = (moneyPoolForRound[r])/(winnersForRound[r].length*2);
             for(uint256 i=0; i<winnersForRound[r].length; i++){
-                (bool rewarded, )= winnersForRound[r][i].call{value: (moneyPoolForRound[r])/winnersForRound[r].length}("");
-                require (rewarded, "Rewarding Failed");
+                if(s_playerGuessForRound[winnersForRound[r][i]][r] == secretNumberOfRound[r]){
+                    (bool rewarded, )= winnersForRound[r][i].call{value: fullReward}("");
+                    require (rewarded, "Rewarding Failed");
+
+                    emit WinnerRewarded(r, winnersForRound[r], fullReward);
+
+                } else {
+                    (bool rewarded, )= winnersForRound[r][i].call{value: halfReward}("");
+                    require (rewarded, "Rewarding Failed");
+
+                    emit WinnerRewarded(r, winnersForRound[r], halfReward);
+                }
             }
         }
     }
